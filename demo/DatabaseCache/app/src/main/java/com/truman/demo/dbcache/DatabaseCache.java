5a18590a4afa2e92f467f28c6997f8d1d6daad0d
Truman <truman.t.kim@gmail.com>
2020-07-15 14:59:59 +0900

    Update DatabaseCache.java
    No issue.

--- demo/DatabaseCache/app/src/main/java/com/truman/demo/dbcache/DatabaseCache.java
+++ demo/DatabaseCache/app/src/main/java/com/truman/demo/dbcache/DatabaseCache.java
@@ -3,7 +3,6 @@ package com.truman.demo.dbcache;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
