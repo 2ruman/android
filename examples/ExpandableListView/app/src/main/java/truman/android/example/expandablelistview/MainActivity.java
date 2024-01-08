8b9a95c1d29119155450335414aec12166e6f22e
Truman <truman.t.kim@gmail.com>
2024-01-08 09:05:13 +0900

    Minor fixes

--- examples/ExpandableListView/app/src/main/java/truman/android/example/expandablelistview/MainActivity.java
+++ examples/ExpandableListView/app/src/main/java/truman/android/example/expandablelistview/MainActivity.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,11 +31,11 @@ public class MainActivity extends AppCompatActivity {
         mBtFunc = findViewById(R.id.bt_func);
         mBtFunc.setOnClickListener((v) -> func());
 
-        MyDataAdapter.OnCheckedListener callback = (groupName, data) -> {
+        MyDataAdapter.OnCheckedListener callback = (groupName, data) ->
             print(String.format(
                     "%s from %s is %s!\n",
                     data.getData(), groupName, data.getState() ? "selected" : "unselected"));
-        };
+
         mAdapter = new MyDataAdapter(this, callback);
 
         mExpandableListView = findViewById(R.id.expandableListView);
@@ -52,22 +50,13 @@ public class MainActivity extends AppCompatActivity {
             print(String.format(
                     "%s collapsed!\n", groupName));
         });
-        mExpandableListView.setOnChildClickListener((p, v, groupPosition, childPosition, id) -> {
-            Toast.makeText(this, "Wow!!!", Toast.LENGTH_SHORT).show();
-            String groupName = mAdapter.getGroupName(groupPosition);
-            MyData data = mAdapter.getData(groupPosition, childPosition);
-            print(String.format(
-                    "%s from %s clicked!\n", data.getData(), groupName));
-            return false;
-        });
+
         Tester.fillData(mAdapter);
         mAdapter.notifyDataSetChanged();
     }
 
     private void print(String s) {
-        runOnUiThread(() -> {
-            mTvStatus.setText(s);
-        });
+        runOnUiThread(() -> mTvStatus.setText(s));
     }
 
     private void test() {
@@ -88,11 +77,10 @@ public class MainActivity extends AppCompatActivity {
             mTvStatus.setText("");
             dataMap.forEach((group, groupData) -> {
                 mTvStatus.append(String .format("[ %s ]\n", group));
-                groupData.forEach((data) -> {
+                groupData.forEach((data) ->
                     mTvStatus.append(String.format(" - %s (%s)\n",
                             data.getData(),
-                            data.getState() ? "v" : " "));
-                });
+                            data.getState() ? "v" : " ")));
             });
         });
     }
