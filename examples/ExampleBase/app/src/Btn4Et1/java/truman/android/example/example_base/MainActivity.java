1e889368b3972d2276cf59a6ae83eb455aa46be3
Truman <truman.t.kim@gmail.com>
2025-06-05 21:11:10 +0900

    ExampleBase: Bump version to 1.2.1  - Enable 'Edge to Edge' as Empty Views Activity provides by default

--- examples/ExampleBase/app/src/Btn4Et1/java/truman/android/example/example_base/MainActivity.java
+++ examples/ExampleBase/app/src/Btn4Et1/java/truman/android/example/example_base/MainActivity.java
@@ -7,7 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import truman.android.example.example_base.databinding.ActivityMainBinding;
 
@@ -25,9 +29,14 @@ public class MainActivity extends AppCompatActivity implements Ui.Out {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         Log.d(TAG, "onCreate()");
 
--- examples/ExampleBase/app/src/Btn5/java/truman/android/example/example_base/MainActivity.java
+++ examples/ExampleBase/app/src/Btn5/java/truman/android/example/example_base/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import truman.android.example.example_base.databinding.ActivityMainBinding;
 
@@ -22,9 +26,14 @@ public class MainActivity extends AppCompatActivity implements Ui.Out {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         Log.d(TAG, "onCreate()");
 
