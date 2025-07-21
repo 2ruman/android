5d3b4f63d5dd8dffafba538b54b6a7a62434c891
Truman <truman.t.kim@gmail.com>
2025-07-22 02:14:40 +0900

    SimpleRuntimeExec: Bump version to 0.2.1  - Expand functions ( synced execution and filtered execution )

--- examples/SimpleRuntimeExec/app/src/main/java/truman/android/example/simpleruntimeexec/ShellCommand.java
+++ examples/SimpleRuntimeExec/app/src/main/java/truman/android/example/simpleruntimeexec/ShellCommand.java
@@ -3,33 +3,74 @@ package truman.android.example.simpleruntimeexec;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.function.Predicate;
 
 public class ShellCommand {
+
+    public static String EMPTY = "";
 
     public static void execute(String command, IShellCommandCallback callback) {
         if (command == null) {
             return;
         }
+        executeInternal(command, callback);
+    }
+
+    public static String execute(String command) {
+        if (command == null) {
+            return EMPTY;
+        }
+        StringWriter sw = new StringWriter();
+        executeInternal(command, (line) -> {
+            sw.append(line);
+            sw.write(System.lineSeparator());
+        });
+        return sw.toString();
+    }
+
+    public static String executeFilteredFirst(String command, Predicate<String> filter) {
+        if (command == null) {
+            return EMPTY;
+        }
+        String filtered = EMPTY;
+        try {
+            executeInternal(command, (line) -> {
+                if (filter.test(line)) {
+                    throw new ExecutionInterruptedException(line);
+                }
+            });
+        } catch (ExecutionInterruptedException e) {
+            filtered = e.reason;
+        }
+        return filtered;
+    }
+
+    private static void executeInternal(String command, IShellCommandCallback callback) {
         try {
             Process proc = Runtime.getRuntime().exec(command);
-            BufferedReader reader;
             String line;
 
-            reader = new BufferedReader(
-                    new InputStreamReader(proc.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                callback.onReadLine(line);
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(proc.getInputStream()))) {
+                while ((line = reader.readLine()) != null) {
+                    callback.onReadLine(line);
+                }
             }
-            reader.close();
 
-            reader = new BufferedReader(
-                    new InputStreamReader(proc.getErrorStream()));
-            while ((line = reader.readLine()) != null) {
-                callback.onReadLine(line);
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(proc.getErrorStream()))) {
+                while ((line = reader.readLine()) != null) {
+                    callback.onReadLine(line);
+                }
             }
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static class ExecutionInterruptedException extends RuntimeException {
+        String reason;
+        ExecutionInterruptedException(String reason) { this.reason = reason; }
     }
 }
